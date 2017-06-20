de6edc4c083bd7c7cf0aeb4a0b57ec85316396d3
LB3MG <lb3mg@radiokameratene.no>
2017-06-20 18:18:34 +0200

    Initial commit.
    Initial mashup. Kink sprinkled but compiling.

---
+++ Zenomat.ino
@@ -0,0 +1,422 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h> //Nick O'Learys from Lib manager
+#include <WiFiManager.h>  //tzapus from Lib manager
+#include <EEPROM.h>
+#include <ArduinoOTA.h>
+
+WiFiClient wclient;
+PubSubClient mqtt(wclient);
+
+//for LED status
+#include <Ticker.h>
+Ticker ticker;
+
+#define   SONOFF_BUTTON             0
+#define   SONOFF_INPUT              14
+#define   SONOFF_LED                13
+#define   SONOFF_RELAY_PIN          12
+
+#define EEPROM_SALT 14579
+#define SONOFF_LED_RELAY_STATE false
+#define HOSTNAME "Zenomat"
+static bool MQTT_ENABLED = true;
+int lastMQTTConnectionAttempt = 0;
+
+const int CMD_WAIT = 0;
+const int CMD_BUTTON_CHANGE = 1;
+
+int cmd = CMD_WAIT;
+//int relayState = HIGH;
+
+//inverted button state
+int buttonState = HIGH;
+
+static long startPress = 0;
+
+typedef struct {
+  char  bootState[3]      = "on";
+  char  mqttHostname[32]  = "test.mosquitto.org";
+  char  mqttPort[6]       = "1883";
+  char  mqttClientID[24]  = "zenomatID";
+  char  mqttTopic[32]     = "relay";
+  int   salt              = EEPROM_SALT;
+} ZenomatSettings;
+
+ZenomatSettings settings;
+
+//http://stackoverflow.com/questions/9072320/split-string-into-string-array
+String getValue(String data, char separator, int index){
+  int found = 0;
+  int strIndex[] = {0, -1};
+  int maxIndex = data.length()-1;
+
+  for(int i=0; i<=maxIndex && found<=index; i++){
+    if(data.charAt(i)==separator || i==maxIndex){
+        found++;
+        strIndex[0] = strIndex[1]+1;
+        strIndex[1] = (i == maxIndex) ? i+1 : i;
+    }
+  }
+  return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
+void tick(){
+  //toggle state
+  int state = digitalRead(SONOFF_LED);  // get the current state of GPIO1 pin
+  digitalWrite(SONOFF_LED, !state);     // set pin to the opposite state
+}
+
+//gets called when WiFiManager enters configuration mode
+void configModeCallback (WiFiManager *myWiFiManager) {
+  Serial.println("Entered config mode");
+  Serial.println(WiFi.softAPIP());
+  //if you used auto generated SSID, print it
+  Serial.println(myWiFiManager->getConfigPortalSSID());
+  //entered config mode, make led toggle faster
+  ticker.attach(0.2, tick);
+}
+
+void updateMQTT() {
+  int state = digitalRead(SONOFF_RELAY_PIN);
+  String stateString;
+  if (state == 1){
+    stateString = "On";
+    } else {
+    stateString = "Off";
+  }
+  int len=stateString.length();
+  char stateChar[len];
+  stateString.toCharArray(stateChar,len);
+  
+  char topic[50];
+  sprintf(topic, "%s/status", settings.mqttTopic);
+  mqtt.publish(topic, stateChar);
+}
+
+void setState(int state) {
+  //relay
+  digitalWrite(SONOFF_RELAY_PIN, state);
+
+  //led
+  if (SONOFF_LED_RELAY_STATE) {
+    digitalWrite(SONOFF_LED, (state + 1) % 2); // led is active low
+  }
+
+  //MQTT
+  updateMQTT();
+
+}
+
+void turnOn(int channel = 0) {
+  int relayState = HIGH;
+  setState(relayState);
+}
+
+void turnOff(int channel = 0) {
+  int relayState = LOW;
+  setState(relayState);
+}
+
+void toggleState() {
+  cmd = CMD_BUTTON_CHANGE;
+}
+
+//flag for saving data
+bool shouldSaveConfig = false;
+
+//callback notifying us of the need to save config
+void saveConfigCallback () {
+  Serial.println("Should save config");
+  shouldSaveConfig = true;
+}
+
+
+void toggle(int channel = 0) {
+  Serial.println("toggle state");
+  Serial.println(digitalRead(SONOFF_RELAY_PIN));
+  int relayState = digitalRead(SONOFF_RELAY_PIN) == HIGH ? LOW : HIGH;
+  setState(relayState);
+}
+
+void restart() {
+  //TODO turn off relays before restarting
+  ESP.reset();
+  delay(1000);
+}
+
+void reset() {
+  //reset settings to defaults
+  //TODO turn off relays before restarting
+  /*
+    WMSettings defaults;
+    settings = defaults;
+    EEPROM.begin(512);
+    EEPROM.put(0, settings);
+    EEPROM.end();
+  */
+  //reset wifi credentials
+  WiFi.disconnect();
+  delay(1000);
+  ESP.reset();
+  delay(1000);
+}
+
+void mqttCallback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is acive low on the ESP-01)
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+/*
+  Serial.print(pub.topic());
+  Serial.print(" => ");
+  if (pub.has_stream()) {
+    int BUFFER_SIZE = 100;
+    uint8_t buf[BUFFER_SIZE];
+    int read;
+    while (read = pub.payload_stream()->read(buf, BUFFER_SIZE)) {
+      Serial.write(buf, read);
+    }
+    pub.payload_stream()->stop();
+    Serial.println("had buffer");
+  } else {
+    Serial.println(pub.payload_string());
+    String topic = pub.topic();
+    String payload = pub.payload_string();
+    
+    if (topic == settings.mqttTopic) {
+      Serial.println("exact match");
+      return;
+    }
+    
+    if (topic.startsWith(settings.mqttTopic)) {
+      Serial.println("for this device");
+      topic = topic.substring(strlen(settings.mqttTopic) + 1);
+      String channelString = getValue(topic, '/', 0);
+      if(!channelString.startsWith("channel-")) {
+        Serial.println("no channel");
+        return;
+      }
+      channelString.replace("channel-", "");
+      int channel = channelString.toInt();
+      Serial.println(channel);
+      if (payload == "on") {
+        turnOn(channel);
+      }
+      if (payload == "off") {
+        turnOff(channel);
+      }
+      if (payload == "toggle") {
+        toggle(channel);
+      }
+      if(payload == "") {
+        updateMQTT(channel);
+      }
+      
+    }
+  }
+  */
+}
+void setup() {
+  Serial.begin(115200);
+
+  //set led pin as output
+  pinMode(SONOFF_LED, OUTPUT);
+  // start ticker with 0.5 because we start in AP mode and try to connect
+  ticker.attach(0.6, tick);
+
+
+  const char *hostname = HOSTNAME;
+
+  WiFiManager wifiManager;
+  //set callback that gets called when connecting to previous WiFi fails, and enters Access Point mode
+  wifiManager.setAPCallback(configModeCallback);
+
+  //timeout - this will quit WiFiManager if it's not configured in 3 minutes, causing a restart
+  wifiManager.setConfigPortalTimeout(180);
+
+  //custom params
+  EEPROM.begin(512);
+  EEPROM.get(0, settings);
+  EEPROM.end();
+
+  if (settings.salt != EEPROM_SALT) {
+    Serial.println("Invalid settings in EEPROM, trying with defaults");
+    ZenomatSettings defaults;
+    settings = defaults;
+  }
+
+
+  WiFiManagerParameter custom_boot_state("boot-state", "on/off on boot", settings.bootState, 33, "<br/>Boot state<br/>");
+  wifiManager.addParameter(&custom_boot_state);
+
+  Serial.println(settings.bootState);
+  Serial.println(settings.mqttHostname);
+  Serial.println(settings.mqttPort);
+  Serial.println(settings.mqttClientID);
+  Serial.println(settings.mqttTopic);
+  
+  WiFiManagerParameter custom_mqtt_text("<br/>MQTT config. <br/> No url to disable.<br/>");
+  wifiManager.addParameter(&custom_mqtt_text);
+
+  WiFiManagerParameter custom_mqtt_hostname("mqtt-hostname", "Hostname", settings.mqttHostname, 33, "<br/>MQTT broker<br/>");
+  wifiManager.addParameter(&custom_mqtt_hostname);
+
+  WiFiManagerParameter custom_mqtt_port("mqtt-port", "port", settings.mqttPort, 6, "<br/>Port<br/>");
+  wifiManager.addParameter(&custom_mqtt_port);
+
+  WiFiManagerParameter custom_mqtt_client_id("mqtt-client-id", "Client ID", settings.mqttClientID, 24, "<br/>Unique ID<br/>");
+  wifiManager.addParameter(&custom_mqtt_client_id);
+
+  WiFiManagerParameter custom_mqtt_topic("mqtt-topic", "Topic", settings.mqttTopic, 33);
+  wifiManager.addParameter(&custom_mqtt_topic);
+
+  //set config save notify callback
+  wifiManager.setSaveConfigCallback(saveConfigCallback);
+
+  if (!wifiManager.autoConnect(hostname)) {
+    Serial.println("failed to connect and hit timeout");
+    //reset and try again, or maybe put it to deep sleep
+    ESP.reset();
+    delay(1000);
+  }
+  
+  //save the custom parameters to FS
+  if (shouldSaveConfig) {
+    Serial.println("Saving config");
+    strcpy(settings.bootState, custom_boot_state.getValue());
+  
+    strcpy(settings.mqttHostname, custom_mqtt_hostname.getValue());
+    strcpy(settings.mqttPort, custom_mqtt_port.getValue());
+    strcpy(settings.mqttClientID, custom_mqtt_client_id.getValue());
+    strcpy(settings.mqttTopic, custom_mqtt_topic.getValue());
+  
+    Serial.println(settings.bootState);
+    EEPROM.begin(512);
+    EEPROM.put(0, settings);
+    EEPROM.end();
+    }
+    
+    //config mqtt
+    if (strlen(settings.mqttHostname) == 0) {
+      MQTT_ENABLED = false;
+    }
+    if (MQTT_ENABLED) {
+    // mqtt.set_server(settings.mqttHostname, atoi(settings.mqttPort));
+    }
+
+    //OTA
+    ArduinoOTA.onStart([]() {
+      Serial.println("Start OTA");
+    });
+    ArduinoOTA.onEnd([]() {
+      Serial.println("\nEnd");
+    });
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    });
+    ArduinoOTA.onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+    ArduinoOTA.setHostname(hostname);
+    ArduinoOTA.begin();
+  
+    //if you get here you have connected to the WiFi
+    Serial.println("connected...yeey :)");
+    ticker.detach();
+  
+    //setup button
+    pinMode(SONOFF_BUTTON, INPUT);
+    attachInterrupt(SONOFF_BUTTON, toggleState, CHANGE);
+
+  //setup relay
+  pinMode(SONOFF_RELAY_PIN, OUTPUT);
+
+   //TODO this should move to last state maybe
+  if (strcmp(settings.bootState, "on") == 0) {
+    turnOn();
+  } else {
+    turnOff();
+  }
+
+  //setup led
+  if (!SONOFF_LED_RELAY_STATE) {
+    digitalWrite(SONOFF_LED, LOW);
+  }
+
+  Serial.println("done setup");
+}
+
+void loop() {
+//ota loop
+  ArduinoOTA.handle();
+
+//mqtt loop
+if (MQTT_ENABLED) {
+  if (!mqtt.connected()) {
+    if(lastMQTTConnectionAttempt == 0 || millis() > lastMQTTConnectionAttempt + 3 * 60 * 1000) {
+      lastMQTTConnectionAttempt = millis();
+      Serial.println(millis());
+      Serial.println("Trying to connect to mqtt");
+      if (mqtt.connect(settings.mqttClientID)) {
+        mqtt.setCallback(mqttCallback);
+        char topic[50];
+        sprintf(topic, "%s/+", settings.mqttTopic);
+        mqtt.subscribe(topic);
+
+        //TODO multiple relays
+        updateMQTT();
+      } else {
+        Serial.println("failed");
+      }
+    }
+    
+  } else {
+    mqtt.loop();
+  }
+}
+
+  //delay(200);
+  //Serial.println(digitalRead(SONOFF_BUTTON));
+  switch (cmd) {
+    case CMD_WAIT:
+      break;
+    case CMD_BUTTON_CHANGE:
+      int currentState = digitalRead(SONOFF_BUTTON);
+      if (currentState != buttonState) {
+        if (buttonState == LOW && currentState == HIGH) {
+          long duration = millis() - startPress;
+          if (duration < 1000) {
+            Serial.println("short press - toggle relay");
+            toggle();
+          } else if (duration < 5000) {
+            Serial.println("medium press - reset");
+            restart();
+          } else if (duration < 60000) {
+            Serial.println("long press - reset settings");
+            reset();
+          }
+        } else if (buttonState == HIGH && currentState == LOW) {
+          startPress = millis();
+        }
+        buttonState = currentState;
+      }
+      break;
+  }
+}
